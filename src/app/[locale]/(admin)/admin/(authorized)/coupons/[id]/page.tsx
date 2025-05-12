d446f12530bc129032deb0df95b434805e077970
Vũ Đức Trường <21520507@gm.uit.edu.vn>
2025-05-12 11:59:38 +0700

    Update page.tsx

--- src/app/[locale]/(admin)/admin/(authorized)/coupons/[id]/page.tsx
+++ src/app/[locale]/(admin)/admin/(authorized)/coupons/[id]/page.tsx
@@ -1,12 +1,9 @@
-'use client'
+"use client";
 import CommonInputOutline from "@/components/common/CommonInputOutline";
 import ValueTypeInput from "@/components/coupon/ValueTypeInput";
 import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
-import {
-  Form,
-  FormField
-} from "@/components/ui/form";
+import { Form, FormField } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
 import { Textarea } from "@/components/ui/textarea";
 import { isoToDatetimeLocal } from "@/lib/date_helper";
@@ -15,28 +12,34 @@ import { zodResolver } from "@hookform/resolvers/zod";
 import { useTranslations } from "next-intl";
 import { usePathname } from "next/navigation";
 import { useForm } from "react-hook-form";
-import React, { useEffect } from 'react'
+import React, { useEffect } from "react";
 import { useShallow } from "zustand/shallow";
 import { useCouponStore } from "@/stores/coupon.store";
 import { useActionToast } from "@/hooks/use-action-toast";
 import LoadingPage from "@/components/special/LoadingPage";
-
-
 
 export default function ConponDetailPage() {
   const t = useTranslations();
   const pathName = usePathname();
   const couponId = pathName.split("/").pop() ?? "0";
 
-  const [selectedCoupon , getCouponById , lastAction , status , error , updateCoupon ] = useCouponStore(useShallow((state) => [
-    state.selectedCoupon,
-    state.getCouponById,
-    state.lastAction,
-    state.status,
-    state.error,
-    state.updateCoupon,
-  ]));
-
+  const [
+    selectedCoupon,
+    getCouponById,
+    lastAction,
+    status,
+    error,
+    updateCoupon,
+  ] = useCouponStore(
+    useShallow((state) => [
+      state.selectedCoupon,
+      state.getCouponById,
+      state.lastAction,
+      state.status,
+      state.error,
+      state.updateCoupon,
+    ])
+  );
 
   useActionToast({
     status,
@@ -44,13 +47,11 @@ export default function ConponDetailPage() {
     errorMessage: error || undefined,
   });
 
-
   useEffect(() => {
     if (couponId) {
       getCouponById(Number(couponId));
     }
-  }
-  , []);
+  }, []);
 
   const form = useForm<CouponUpdate>({
     mode: "onSubmit",
@@ -67,8 +68,7 @@ export default function ConponDetailPage() {
     }
   }, [selectedCoupon]);
 
-
-  if(!selectedCoupon) {
+  if (!selectedCoupon) {
     return LoadingPage();
   }
 
@@ -77,42 +77,42 @@ export default function ConponDetailPage() {
 
     form.handleSubmit(async (data) => {
       console.log(data);
-      updateCoupon(data)
+      updateCoupon(data);
     })();
   };
   return (
     <Card>
       <CardHeader>
         <CardTitle>
-          <h2>{t("coupon_x", {x: couponId})}</h2>
+          <h2>{t("coupon_x", { x: couponId })}</h2>
         </CardTitle>
       </CardHeader>
       <CardContent>
         <Form {...form}>
-          <form onSubmit={handleSubmit} className="grid grid-cols-3 gap-y-8 gap-x-10">
+          <form
+            onSubmit={handleSubmit}
+            className="grid grid-cols-3 gap-y-8 gap-x-10"
+          >
             <FormField
               name="code"
               control={form.control}
               render={({ field }) => (
-                <CommonInputOutline title={t('coupon_code')}>
+                <CommonInputOutline title={t("coupon_code")}>
                   <Input {...field} />
                 </CommonInputOutline>
               )}
             />
 
             <div className="col-span-2">
-            <ValueTypeInput />
+              <ValueTypeInput />
             </div>
 
             <FormField
               name="availableFrom"
               control={form.control}
               render={({ field }) => (
-                <CommonInputOutline title={t("available_from")}>	
-                  <Input
-                    {...field}
-                    type="datetime-local"
-                  />
+                <CommonInputOutline title={t("available_from")}>
+                  <Input {...field} type="datetime-local" />
                 </CommonInputOutline>
               )}
             />
@@ -122,10 +122,7 @@ export default function ConponDetailPage() {
               control={form.control}
               render={({ field }) => (
                 <CommonInputOutline title={t("available_to")}>
-                  <Input
-                    {...field}
-                    type="datetime-local"
-                  />
+                  <Input {...field} type="datetime-local" />
                 </CommonInputOutline>
               )}
             />
@@ -136,11 +133,8 @@ export default function ConponDetailPage() {
               name="minAmount"
               control={form.control}
               render={({ field }) => (
-                <CommonInputOutline title={t("min_amount")}>	
-                  <Input
-                    {...field}
-                    type="number"
-                  />
+                <CommonInputOutline title={t("min_amount")}>
+                  <Input {...field} type="number" />
                 </CommonInputOutline>
               )}
             />
@@ -148,11 +142,17 @@ export default function ConponDetailPage() {
             <FormField
               name="maxAppliedAmount"
               control={form.control}
+              disabled={form.watch("type") === "FIXED"}
               render={({ field }) => (
                 <CommonInputOutline title={t("max_applied_amount")}>
                   <Input
                     {...field}
                     type="number"
+                    value={
+                      form.watch("type") === "FIXED"
+                        ? form.watch("value")
+                        : field.value
+                    }
                   />
                 </CommonInputOutline>
               )}
@@ -163,10 +163,7 @@ export default function ConponDetailPage() {
               control={form.control}
               render={({ field }) => (
                 <CommonInputOutline title={t("max_uses")}>
-                  <Input
-                    {...field}
-                    type="number"
-                  />
+                  <Input {...field} type="number" />
                 </CommonInputOutline>
               )}
             />
@@ -175,13 +172,18 @@ export default function ConponDetailPage() {
               name="description"
               control={form.control}
               render={({ field }) => (
-                <CommonInputOutline title={t("Description")} className="col-span-3">
+                <CommonInputOutline
+                  title={t("Description")}
+                  className="col-span-3"
+                >
                   <Textarea {...field} />
                 </CommonInputOutline>
               )}
             />
 
-            <Button className="col-start-3 bg-green-400 hover:bg-green-500">{t("update_coupon")}</Button>
+            <Button className="col-start-3 bg-green-400 hover:bg-green-500">
+              {t("update_coupon")}
+            </Button>
           </form>
         </Form>
       </CardContent>
