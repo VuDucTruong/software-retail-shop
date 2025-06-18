d4fc828e5296dd6a76f9c8aa18a5db69b1915fda
Vũ Đức Trường <21520507@gm.uit.edu.vn>
2025-06-18 21:41:47 +0700

    update docker

--- src/app/[locale]/chat/route.ts
+++ src/app/[locale]/chat/route.ts
@@ -4,7 +4,7 @@ import { NextRequest, NextResponse } from 'next/server'
 export async function POST(req: NextRequest) {
   const body = await req.json()
   try {
-    const response = await fetch('http://localhost:3001/api/v1/prediction/42ada514-7c9c-4568-a394-66716ce7319c', {
+    const response = await fetch('http://localhost:3001/api/v1/prediction/cb62b6bb-244b-426b-b4e0-31ead46bcbdb', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
