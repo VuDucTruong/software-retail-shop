6088e10f48410ea871070c249f9718b1baf1fc0f
Vũ Đức Trường <21520507@gm.uit.edu.vn>
2025-06-27 16:45:42 +0700

    add leading header

--- src/components/blog/BlogNavMenu.tsx
+++ src/components/blog/BlogNavMenu.tsx
@@ -3,7 +3,7 @@
 import * as React from "react";
 import Link from "next/link";
 
-import { cn } from "@/lib/utils";
+import Image from "next/image";
 import {
   NavigationMenu,
   NavigationMenuContent,
@@ -16,8 +16,11 @@ import {
 import BlogSearchBar from "./BlogSearchBar";
 import { GenreDomain } from "@/stores/blog/genre.store";
 import { useShallow } from "zustand/shallow";
-import { BLOG_Q_PARAMS } from "@/lib/constants";
+import { BLOG_Q_PARAMS, languages } from "@/lib/constants";
 import { useTranslations } from "next-intl";
+import { usePathname, useRouter } from "next/navigation";
+import { Separator } from "../ui/separator";
+import { CommonSelect } from "../common/CommonSelect";
 
 type GenreBase = {
   id: number;
@@ -65,52 +68,101 @@ export default function BlogNavMenu() {
   const t = useTranslations();
   const menuItems: Genre[] = fromGenreDomainToMenuItem(genre1s);
 
+  const router = useRouter();
+  const pathname = usePathname();
+
+  const switchLanguage = (locale: string) => {
+    const newPath = `/${locale}${pathname.substring(3)}`; // Change locale in the URL
+    router.replace(newPath);
+  };
+
+  const supportLinks = [
+    { name: "about_us", href: "/" },
+    { name: "contact_us", href: "/" },
+    { name: "Support", href: "/feedback" },
+  ];
+
   return (
-    <div className="flex flex-row items-center justify-between">
-      <NavigationMenu>
-        <NavigationMenuList>
-          <NavigationMenuItem>
-            <NavigationMenuLink
-              href={"/blog"}
-              className={navigationMenuTriggerStyle()}
-            >
-              {t("Home")}
-            </NavigationMenuLink>
-          </NavigationMenuItem>
-          {menuItems.map((item, index) => (
-            <React.Fragment key={index}>
-              {item.children.length > 0 ? (
-                <NavigationMenuItem>
-                  <NavigationMenuTrigger>{item.title}</NavigationMenuTrigger>
-                  <NavigationMenuContent>
-                    <ul className="grid gap-2 p-2 md:w-[400px] lg:w-[500px] lg:grid-cols-[.75fr_1fr]">
-                      {item.children.map((subItem, subIndex) => (
-                        <ListItem
-                          title={subItem.title}
-                          href={subItem.href ?? "/blog"}
-                          key={subIndex}
-                        >
-                          
-                        </ListItem>
-                      ))}
-                    </ul>
-                  </NavigationMenuContent>
-                </NavigationMenuItem>
-              ) : (
-                <NavigationMenuItem>
-                  <NavigationMenuLink
-                    href={item.href ?? ""}
-                    className={navigationMenuTriggerStyle()}
-                  >
-                    {item.title}
-                  </NavigationMenuLink>
-                </NavigationMenuItem>
-              )}
-            </React.Fragment>
+    <div className="flex flex-col">
+      <section className="flex justify-center items-center border-b border-border">
+        <div className="flex flex-row space-x-4 italic flex-1">
+          {supportLinks.map((value, index) => (
+            <div className="flex flex-row items-center space-x-4" key={index}>
+              <Link
+                className="hover:underline hover:text-primary"
+                key={index}
+                href={value.href}
+              >
+                {t(value.name)}{" "}
+              </Link>
+              <Separator orientation="vertical" />
+            </div>
           ))}
-        </NavigationMenuList>
-      </NavigationMenu>
-      <BlogSearchBar />
+        </div>
+
+        <div className="flex flex-row">
+          <CommonSelect
+            data={languages}
+            defaultValue={languages[0]}
+            onChange={(value) => {
+              switchLanguage(value);
+            }}
+          />
+          <div className="py-1">
+            <Separator orientation="vertical" />
+          </div>
+        </div>
+      </section>
+
+      <div className="flex w-fit">
+        <Link href="/">
+          <Image src={"/logo.png"} alt="LOGO" width={150} height={150} />
+        </Link>
+      </div>
+      <div className="flex flex-row items-center justify-between">
+        <NavigationMenu>
+          <NavigationMenuList>
+            <NavigationMenuItem>
+              <NavigationMenuLink
+                href={"/blog"}
+                className={navigationMenuTriggerStyle()}
+              >
+                {t("Home")}
+              </NavigationMenuLink>
+            </NavigationMenuItem>
+            {menuItems.map((item, index) => (
+              <React.Fragment key={index}>
+                {item.children.length > 0 ? (
+                  <NavigationMenuItem>
+                    <NavigationMenuTrigger>{item.title}</NavigationMenuTrigger>
+                    <NavigationMenuContent>
+                      <ul className="grid gap-2 p-2 md:w-[400px] lg:w-[500px] lg:grid-cols-[.75fr_1fr]">
+                        {item.children.map((subItem, subIndex) => (
+                          <ListItem
+                            title={subItem.title}
+                            href={subItem.href ?? "/blog"}
+                            key={subIndex}
+                          ></ListItem>
+                        ))}
+                      </ul>
+                    </NavigationMenuContent>
+                  </NavigationMenuItem>
+                ) : (
+                  <NavigationMenuItem>
+                    <NavigationMenuLink
+                      href={item.href ?? ""}
+                      className={navigationMenuTriggerStyle()}
+                    >
+                      {item.title}
+                    </NavigationMenuLink>
+                  </NavigationMenuItem>
+                )}
+              </React.Fragment>
+            ))}
+          </NavigationMenuList>
+        </NavigationMenu>
+        <BlogSearchBar />
+      </div>
     </div>
   );
 }
@@ -125,9 +177,11 @@ function ListItem({
     <li {...props}>
       <NavigationMenuLink asChild>
         <Link href={href}>
-          <div className="text-sm leading-none font-medium p-2 hover:text-primary">{title}</div>
+          <div className="text-sm leading-none font-medium p-2 hover:text-primary">
+            {title}
+          </div>
         </Link>
       </NavigationMenuLink>
     </li>
-  )
+  );
 }
