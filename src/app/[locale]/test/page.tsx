6373eb4cf1ad6efd94a96bd08a7fa653cbd81411
Vũ Đức Trường <21520507@gm.uit.edu.vn>
2025-06-30 00:34:10 +0700

    create sample test

---
+++ src/app/[locale]/test/page.tsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import * as Sentry from "@sentry/nextjs";
+
+export default function page() {
+
+
+    const handleThrowError = () => {
+        try {
+            throw new Error("This is a test error");
+        } catch (error) {
+            Sentry.captureException(error, {
+                extra: {
+                    info: "This is a test error for Sentry",
+                },
+            });
+        }
+    }
+
+  return (
+    <div className="flex flex-col items-center justify-center h-screen">
+      <h1 className="text-2xl font-bold mb-4">Sentry Test Page</h1>
+      <button
+        onClick={handleThrowError}
+        className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600"
+      >
+        Throw Error
+      </button>
+    </div>
+  )
+}
